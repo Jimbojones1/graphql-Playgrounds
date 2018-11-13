f80c8d04eeaa0ecd69d49678c9a4780c9114ec90
Jim Haff <Jimmyhaff3@gmail.com>
2018-11-13 17:55:06 +0000

    initial setup

---
+++ apollo-intro/src/Contacts.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Contacts = () => {
+  return (
+    <ul>
+      <li>Larry David</li>
+      <li>Jerry Seinfeld</li>
+    </ul>
+    )
+}
+
+export default Contacts;
